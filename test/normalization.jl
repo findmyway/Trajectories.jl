bc73f3462919e86f08ea29551f0a2fad3c20b2de
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2022-05-19 11:39:20 +0200

    tests

--- test/normalization.jl
+++ test/normalization.jl
@@ -1,47 +1,48 @@
 using Test
 using Trajectories
-import Trajectories.normalize!
+import Trajectories.normalize
 import OnlineStats: fit!, mean, std
 
-#@testset "normalization.jl" begin
+@testset "normalization.jl" begin
     #scalar normalization
     rewards = [1.:10;]
-    rn = reward_normalizer()
+    rn = scalar_normalizer()
     fit!(rn, rewards)
     batch_reward = [6.,5.,10.]
     output = normalize!(rn, batch_reward)
     @test batch_reward == output != [6.,5.,10.]
     #vector normalization
     states = reshape([1:50;], 5, 10)
-    sn = state_normalizer((5,))
-    fit!(sn, eachslice(states; dims = ndims(states)))
+    sn = array_normalizer((5,))
+    fit!(sn, states)
     @test [mean(stat) for stat in sn] == [mean((1:5:46) .+i) for i in 0:4]
     batch_states = reshape(repeat(5.:-1:1, 5), 5,5)
     normalize!(sn, batch_states)
     @test all(length(unique(x)) == 1 for x in eachrow(batch_states))
     #array normalization
     states = reshape(1.:250, 5,5,10)
-    sn = state_normalizer((5,5))
+    sn = array_normalizer((5,5))
     fit!(sn, eachslice(states, dims = 3))
     batch_states = collect(states)
     normalize!(sn, batch_states)
     
-    #NormalizedTrajectory
+    #NormalizedTrace
     t = Trajectory(
         container=Traces(
-            a=Float32[],
-            b=Int[]
+            a= NormalizedTrace(Float32[], scalar_normalizer()),
+            b=Int[],
+            c=NormalizedTrace(Vector{Float32}[], array_normalizer((10,))) #TODO check with ElasticArrays and Episodes
         ),
         sampler=BatchSampler(30000),
         controler=InsertSampleRatioControler(Inf, 0)
     )
-    nt = NormalizedTrajectory(t, :a => reward_normalizer())
-    append!(nt, a = [1,2,3], b = [1,2,3])
-    push!(nt, a = 2, b = 2)
-    @test mean(nt.normalizer[:a]) ≈ 2.
-    @test std(nt.normalizer[:a]) ≈ std([1,2,2,3])
-    a,b = take!(nt)
+    append!(t, a = [1,2,3], b = [1,2,3], c = eachcol(reshape(1f0:30, 10,3)))
+    push!(t, a = 2, b = 2, c = fill(mean(1:30), 10))
+    @test mean(t.container[:a].trace.x) ≈ 2.
+    @test std(t.container[:a].trace.x) ≈ std([1,2,2,3])
+    a,b,c = take!(t)
     @test eltype(a) == Float32
     @test mean(a) ≈ 0 atol = 0.01
     @test mean(b) ≈ 2 atol = 0.01 #b is not normalized
+    @test all(isapprox(0f0, atol = 0.01), vec(mean(reduce(hcat,c), dims = 2)))
 end
