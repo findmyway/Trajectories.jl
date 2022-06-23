0a27ae062755fb5c271dc543063b55824244cac2
Henri Dehaybe <47037088+HenriDeh@users.noreply.github.com>
2022-06-23 16:46:50 +0200

    add a warning to sampler docs

--- src/samplers.jl
+++ src/samplers.jl
@@ -55,6 +55,10 @@ export MetaSampler
 Wraps a NamedTuple containing multiple samplers. When sampled, returns a named tuple with a 
 batch from each sampler.
 Used internally for algorithms that sample multiple times per epoch.
+Note that a single "sampling" with a MetaSampler only increases the Trajectory controler 
+count by 1, not by the number of internal samplers. This should be taken into account when
+initializing an agent.
+
 
 # Example
 ```
@@ -80,6 +84,9 @@ export MultiBatchSampler
 
 Wraps a sampler. When sampled, will sample n batches using sampler. Useful in combination 
 with MetaSampler to allow different sampling rates between samplers.
+Note that a single "sampling" with a MultiBatchSampler only increases the Trajectory 
+controler count by 1, not by `n`. This should be taken into account when
+initializing an agent.
 
 # Example
 ```
